d5b0f4263f9260a8230522fdaee416fc16b1735d
George Moe <geomo3@gmail.com>
2017-12-02 20:29:08 -0500

    Multiscale experiments.

--- eyebis/eyebis.py
+++ eyebis/eyebis.py
@@ -29,6 +29,7 @@ models = {
                     "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
                     "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
                     "sofa", "train", "tvmonitor"],
+        'shape': (300, 300)
     },
 
     # SSD300_VGG
@@ -42,6 +43,7 @@ models = {
                     "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
                     "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
                     "sofa", "train", "tvmonitor"],
+        'shape': (224, 224)
     },
 }
 
@@ -54,7 +56,8 @@ parameters = {
     'video_source': 0,  # Use 0 for webcam
     # 'video_source': "aerial.mp4",
     # 'reshape': (640, 360),
-    'reshape': (300, 300),
+    # 'reshape': (300, 300),
+    'stride': 300,
     'targets': ['car', 'person']
 }
 
@@ -76,56 +79,75 @@ cv2.namedWindow('VISION', cv2.WINDOW_NORMAL)
 
 # loop over the frames from the video stream
 while True:
-    # grab the frame from the threaded video stream and resize it
-    # to have a maximum width of 400 pixels
-    frame = vs.read()
-    frame = imutils.resize(frame, width=400)
+    # grab the frame from the threaded video stream
+    frame = cv2.flip(vs.read(), 1)
+
+    print(frame.shape[:2])
+    # frame = imutils.resize(frame, width=400)
 
     # grab the frame dimensions and convert it to a blob
     (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, parameters['reshape']),
-                                 0.007843, parameters['reshape'], model['bgr_sub'])
 
-    # pass the blob through the network and obtain the detections and
-    # predictions
-    net.setInput(blob)
-    detections = net.forward()
-    detections = model['normalize'](detections)
+    all_detections = []
+    for x_slice in range(0, 300, parameters['stride']):
+        for y_slice in range(0, 300, parameters['stride']):
+            print("{}-{}x/{} {}-{}y/{}".format(x_slice, x_slice + parameters['stride'], w, y_slice,
+                  y_slice + parameters['stride'], h))
+
+            blob = cv2.dnn.blobFromImage(
+                cv2.resize(frame[x_slice:x_slice + model['shape'][0], y_slice:y_slice + model['shape'][1]],
+                           model['shape']),
+                0.007843, model['shape'], model['bgr_sub'])
+
+            # pass the blob through the network and obtain the detections and
+            # predictions
+            net.setInput(blob)
+            detections = net.forward()
+            detections = model['normalize'](detections)
+            all_detections.append((x_slice, y_slice, detections))
 
     # loop over the detections
-    for i in range(detections.shape[0]):
-        # extract the confidence (i.e., probability) associated with
-        # the prediction
-        confidence = detections[i, 2]
+    for x_slice, y_slice, detections in all_detections:
 
-        # filter out weak detections by ensuring the `confidence` is
-        # greater than the minimum confidence
-        if confidence > parameters['min_confidence']:
-            # extract the index of the class label from the
-            # `detections`, then compute the (x, y)-coordinates of
-            # the bounding box for the object
-            idx = int(detections[i, 1])
-            box = detections[i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+        cv2.rectangle(frame, (x_slice, y_slice), (x_slice + 300, y_slice + 300), (255, 255, 255), 2)
 
-            name = model['classes'][idx] if idx < len(model['classes']) else "unknown ({})".format(idx)
+        for i in range(detections.shape[0]):
+            # extract the confidence (i.e., probability) associated with
+            # the prediction
+            confidence = detections[i, 2]
 
-            if name not in parameters['targets']:
-                continue
+            # filter out weak detections by ensuring the `confidence` is
+            # greater than the minimum confidence
+            if confidence > parameters['min_confidence']:
+                # extract the index of the class label from the
+                # `detections`, then compute the (x, y)-coordinates of
+                # the bounding box for the object
+                idx = int(detections[i, 1])
+                box = detections[i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                # startX += x_slice
+                # endX += x_slice
+                # startY += y_slice
+                # endY += y_slice
 
-            centroid = ((startX + endX) / 2, (startY + endY) / 2)
-            label = "{}: {:.2f}% @({}, {})".format(name,
-                                                   confidence * 100, *centroid)
+                name = model['classes'][idx] if idx < len(model['classes']) else "unknown ({})".format(idx)
 
-            # draw the prediction on the frame
-            if not idx in colors:
-                colors[idx] = np.random.uniform(0, 255, size=3)
-            color = colors[idx]
+                if name not in parameters['targets']:
+                    continue
 
-            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-            y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(frame, label, (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                centroid = ((startX + endX) / 2, (startY + endY) / 2)
+                label = "{}: {:.2f}% @({}, {})".format(name,
+                                                       confidence * 100, *centroid)
+
+                # draw the prediction on the frame
+                if not idx in colors:
+                    colors[idx] = np.random.uniform(0, 255, size=3)
+                color = colors[idx]
+
+                cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(frame, label, (startX, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
     # show the output frame
     cv2.imshow("VISION", frame)
